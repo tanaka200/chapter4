1fa41c48f564db7b8215135739537e6e271c6f7a
tanaka200 <131841182+tanaka200@users.noreply.github.com>
2023-11-06 11:22:26 +0900

    Initial commit

---
+++ servo.py
@@ -0,0 +1,25 @@
+﻿import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2, GPIO.OUT)
+servo = GPIO.PWM(2, 50)
+servo.start(0.0)
+
+bottom = 2.5  # -90deg : 0.5ms / 20ms = 2.5[%]
+middle = 7.25 #   0deg : 1.45ms / 20ms = 7.25[%]
+top = 12.0    #  90deg : 2.4ms / 20ms = 12.0[%]
+
+for i in range(5):
+	servo.ChangeDutyCycle(bottom)
+	time.sleep(1.0)
+
+	servo.ChangeDutyCycle(middle)
+	time.sleep(1.0)
+
+	servo.ChangeDutyCycle(top)
+	time.sleep(1.0)
+
+
+
+
